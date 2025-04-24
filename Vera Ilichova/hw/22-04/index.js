1e4a3eebef366edd775108bc938aeebb7f8834a0
Vera <ilichovav@gmail.com>
2025-04-24 20:05:12 +0200

    First Homework

--- Vera Ilichova/hw/22-04/index.js
+++ Vera Ilichova/hw/22-04/index.js
@@ -1,8 +1,20 @@
-// minimum
-let minNumber = 0; let massiv = [];
-for (let i = 1; i < 10; i++) {
-    if (i === 1) { minNumber = massiv[i]; continue; }
+// minimumco
+//const massiv=[4,7,1,3,9,3,6,2,6,9];
+let minNumber = 0;
+for (let i = 0; i < massiv.length; i++) {
+    if (i === 0) { minNumber = massiv[i]; continue; }
     else if (minNumber < massiv[i]) { minNumber = massiv[i]; }
-    else { }
-};
+    };
 console.log(minNumber);
+// 1. Дан массив [4,7,1,3,9,3,6,2,6,9]
+
+// вывести в консоль КОЛИЧЕСТВО (штук) четных чисел из этого массива
+
+// 
+ const massiv=[4,7,1,3,9,3,6,2,6,9];
+let j = 0; let k=0;
+for (let i = 0; i < massiv.length; i++) {
+    if ((massiv [i] % 2) === 0) { k=k+1; console.log(`количество четных чисел ${k} `);}
+       
+    else  {j=j+1;console.log(`количество нечетных чисел ${j}`);}
+    };
