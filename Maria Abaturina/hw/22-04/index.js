5113b7db95d85763f323c5e2e7e1c71e0d34ce2c
Maria <abaturinamary@gmail.com>
2025-04-24 19:41:51 +0200

    changes hw/

--- Maria Abaturina/hw/22-04/index.js
+++ Maria Abaturina/hw/22-04/index.js
@@ -1,23 +1,25 @@
 // 1. Дан массив [4,7,1,3,9,3,6,2,6,9]
 // вывести в консоль КОЛИЧЕСТВО (штук) четных чисел из этого массива
 
-// const arr = [4,7,1,3,9,3,6,2,6,9];
-// let arr2 = 0; 
-// for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] % 2 ===0) {
-//         arr2++;
+// const array = [4,7,1,3,9,3,6,2,6,9];
+// let array2 = 0; 
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 ===0) {
+//         array2++;
 //     } 
-// }console.log("кол.во штук", arr2);
+// }
+// console.log("кол.во штук", arr2);
 // 2. вывести самое наименьшее число
 
-let arr = [4,7,1,3,9,3,6,2,6,9];
-let min = arr[0];
+let array = [4,7,1,3,9,3,6,2,6,9];
+let minNumber = array[0];
 
-for(let i = 1; i < arr.length; i++){
-    if (arr[i] < min){
-        min = arr[i];
+for(let i = 1; i < array.length; i++){
+    if (array[i] < min){
+        minNumber = array[i];
     }
-}console.log("мин.число",min);
+}
+console.log("мин.число",minNumber);
 
 
 
