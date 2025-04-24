2c0662420c38e4325213b16d5850c5e5cdf6ba5a
Vera <ilichovav@gmail.com>
2025-04-24 22:21:19 +0200

    Final correction

--- Vera Ilichova/hw/22-04/index.js
+++ Vera Ilichova/hw/22-04/index.js
@@ -4,16 +4,22 @@
 //    if (i === 0) { minNumber = massiv[i]; continue; }
 //   else if (minNumber > massiv[i]) { minNumber = massiv[i]; }
 //}
-console.log(minNumber);
+//console.log(minNumber);
 // 1. Дан массив [4,7,1,3,9,3,6,2,6,9]
 
 // вывести в консоль КОЛИЧЕСТВО (штук) четных чисел из этого массива
 
-// 
+//
 const massiv = [4, 7, 1, 3, 9, 3, 6, 2, 6, 9];
-let j = 0; let k = 0;
+let j = 0;
+let k = 0;
 for (let i = 0; i < massiv.length; i++) {
-    if ((massiv[i] % 2) === 0) { k = k + 1; console.log(`количество четных чисел ${k} `); }
-
-    else { j = j + 1; console.log(`количество нечетных чисел ${j}`); }
+  if (massiv[i] % 2 === 0) {
+    k = k + 1;
+    console.log(`количество четных чисел ${k} `);
+  } else {
+    j = j + 1;
+    console.log(`количество нечетных чисел ${j}`);
+  }
 }
+//
