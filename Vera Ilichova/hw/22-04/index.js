2eb0f1c4411ed95fbbaf6f4375ef270ec27cd227
Vera <ilichovav@gmail.com>
2025-05-06 19:09:22 +0200

    HW 22-04

--- Vera Ilichova/hw/22-04/index.js
+++ Vera Ilichova/hw/22-04/index.js
@@ -1,27 +1,3 @@
-// minimum
-//const massiv = [4, 7, 1, 3, 9, 3, 6, 2, 6, 9];
-//for (let i = 0; i < massiv.length; i++) {
-//    if (i === 0) { minNumber = massiv[i]; continue; }
-//   else if (minNumber > massiv[i]) { minNumber = massiv[i]; }
-//}
-//console.log(minNumber);
-// 1. Дан массив [4,7,1,3,9,3,6,2,6,9]
-
-// вывести в консоль КОЛИЧЕСТВО (штук) четных чисел из этого массива
-
-//
-//const massiv = [4, 7, 1, 3, 9, 3, 6, 2, 6, 9];
-//let j = 0;
-//let k = 0;
-//for (let i = 0; i < massiv.length; i++) {
-//  if (massiv[i] % 2 === 0) {
-//    k = k + 1;
-//    console.log(`количество четных чисел ${k} `);
-//  } else {
-//    j = j + 1;
-//    console.log(`количество нечетных чисел ${j}`);
-  }
-}
 //Дан массив `[3,4,7,1,9,3,7,2,5,6]`. Найдите среднее арифметическое значение всех элементов массива, которые имеют четный индекс.
 const massiv = [3, 4, 7, 1, 9, 3, 7, 2, 5, 6];
 let result = 0;
@@ -31,12 +7,35 @@ for (let i = 0; i < massiv.length; i++) {
     result = result + massiv[i];
     indexChet++;
   }
-  console.log(`четный индекс ${indexChet} `);
 }
 result = result / indexChet;
-console.log(` ${k} `);
 console.log(
-  `среднее арифметическое значение всех элементов массива, которые имеют четный индекс ${result} `
+  `среднее арифметическое элементов массива с четным индексом ${result} `
 );
+
 //2. Сформировать массив длиной 10, в который с помощью цикла запушить только те числа которые делятся на 3 без остатка.
+let arr = [];
+for (;;) {
+  userNumber = Number(prompt("введи число"));
+  if (userNumber % 3 === 0) {
+    arr.push(userNumber);
+    if (arr.length === 10) {
+      console.log(arr);
+      break;
+    }
+  }
+}
+
 //3. Дан массив чисел `[3,5,2,6,8,1,7]`, показать этот массив пользователю с помощью `alert()`, после чего попросить ввести те же числа с помощью `prompt()` по памяти, если все числа совпали, то выводим `alert()` с поздравлениям, если не совпало хотя бы одно число, сразу выводим `alert()` с ошибкой
+const arr1 = [3, 5, 2, 6, 8, 1, 7];
+alert(arr1);
+for (let i = 0; i < arr1.length; i++) {
+  userNumber = Number(prompt(`input number`));
+  if (userNumber === arr1[i]) {
+    continue;
+  } else {
+    alert(`error`);
+    break;
+  }
+  alert(`Congratulations!`);
+}
