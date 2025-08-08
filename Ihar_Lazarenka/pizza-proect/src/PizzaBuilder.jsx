8cbbece26f1aaaf38fa6cd9e654da467a537cd88
IharLazareka1986 <igarlazarenka@gmail.com>
2025-08-08 22:06:06 +0200

    PIZZA

---
+++ Ihar_Lazarenka/pizza-proect/src/PizzaBuilder.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PizzaOrder from "./PizzaOrder";
+
+const PizzaBuilder = () => {
+  return (
+    <div className="pizza-builder animate-fade-in-up">
+      <PizzaOrder />
+    </div>
+  );
+};
+
+export default PizzaBuilder;
--- Prepod/lesson16/test.js
+++ Prepod/lesson16/test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import {one} from './blabla/one.js';
 
@@ -8,3 +9,15 @@ console.log('test.js', one);
 
 export const a = 2;
 
+=======
+
+import {one} from './blabla/one.js';
+
+const myOne = {...one};
+const myOne2 = structuredClone(one);
+
+console.log('test.js', one);
+
+export const a = 2;
+
+>>>>>>> 345f401 (PIZZA)
